6d0365cd3f3335d570bee9ec8502430eb9db3ee0
HeQiuting <45375773+HeQiuting@users.noreply.github.com>
2018-12-14 15:01:18 +0800

    Update BucketSort.java

--- BucketSort.java
+++ BucketSort.java
@@ -1,1 +1,19 @@
+1.先便利数组，确定最大值
+2.确定长度：最大值+1
+3.创造新的数组，计数：每找到一次数，在相对应地数组上+1
+4.输出数组里不为0的
+int temp = 0;
+for(int i = 0; i < a.length; i++){
+  if(a[i] > temp){
+    temp = a[i];
+  }
+}
+int[] b = new int[temp+1];
+for(int n = 0; n < b.length; n++){
+  if(a[i] == n){
+    b[n]++;
+  }
+}
 
+
+  
